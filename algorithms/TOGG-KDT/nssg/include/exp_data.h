b6ca949af53d0aaf6917e1820faab616f74622cc
k16diablo <miguel92@naver.com>
2022-10-23 01:41:40 +0900

    TOGG-KDT update

--- algorithms/TOGG-KDT/nssg/include/exp_data.h
+++ algorithms/TOGG-KDT/nssg/include/exp_data.h
@@ -47,7 +47,7 @@ void set_para(std::string dataset, efanna2e::Parameters &parameters) {
         std::cout << "input dataset error!\n";
         exit(-1);
     }
-    std::string nn_graph_path("/ddisk/wmz/TOGG/algorithms/GA/efanna_graph/build/tests/" + dataset + "_knn.graph");
+    std::string nn_graph_path("../../../../GA/efanna_graph/build/tests/" + dataset + "_knn.graph");
     parameters.Set<unsigned>("L", L);
     parameters.Set<unsigned>("R", R);
     parameters.Set<unsigned>("A", A);
@@ -57,7 +57,7 @@ void set_para(std::string dataset, efanna2e::Parameters &parameters) {
 
 void set_data_path(std::string dataset, std::string &base_path, std::string &query_path, std::string &ground_path) {
     // dataset root path
-    std::string dataset_root = "/home_bak/yq/ANNS/dataset/";
+    std::string dataset_root = "../../../../../routing_evaluation/dataset/";
     base_path = dataset_root;
     query_path = dataset_root;
     ground_path = dataset_root;
